1193593bf9667a01e695b9c4c5c72644e9643ca4
0xA2 <36355541+0xA2@users.noreply.github.com>
2021-03-17 18:11:37 +0000

    Update list.h

--- list.h
+++ list.h
@@ -1,8 +1,9 @@
 #include "card.h"
-#ifndef _LIST_H_
+#ifndef LIST_H
+#define LIST_H
 
 typedef struct lnode {
-  card task;
+  card* task;
   struct lnode *next;
 } node;
 
@@ -11,22 +12,25 @@ typedef struct {
   node *first;
 } list;
 
-node *nodeNew(card, node *);
-list *listNew();
-int listIsEmpty(list *);
-void listDestroy(list *);
-void listAddFirst(card, list *);
-void listAddLast(card, list *);
-card listGetFirst(list *);
-card listGetLast(list *);
-void listRemoveFirst(list *);
-void listRemoveLast(list *);
-int listValueAtIndex(int, list *);
-void listRemoveAtIndex(int, list *);
-void listAddAtIndex(int, card, list *);
-void listChangeAtIndex(int, card, list *);
-int listSize(list *);
-int listContains(card, list *);
-void listPrint(list *);
+node* nodeNew(card*, node*);
+list* listNew();
+int listIsEmpty(list*);
+void listDestroy(list*);
+void listAddFirst(card*, list*);
+void listAddLast(card*, list*);
+card* listGetFirst(list*);
+card* listGetLast(list*);
+void listRemoveFirst(list*);
+void listRemoveLast(list*);
+int listValueAtIndex(int, list*);
+void listRemoveAtIndex(int, list*);
+void listAddAtIndex(int, card*, list*);
+void listChangeAtIndex(int, card*, list*);
+int listSize(list*);
+int listContains(card*, list*);
+list* loadToDo();
+list* loadDoing();
+list* loadDone();
+void listPrint(list*);
 
-#endif //_LIST_H_
+#endif
