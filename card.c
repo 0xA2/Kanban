8faf914cb0ece3652e2d97d43b9b47ee13e51067
0xA2 <36355541+0xA2@users.noreply.github.com>
2021-03-18 19:26:31 +0000

    Update card.c

--- card.c
+++ card.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "card.h"
 
-card *cardNew(int id, int priority, char *desc) {
-  card *l = (card *) malloc(sizeof(card));
+card* cardNew(int i, int pri, long time ,char *desc) {
+  card *l = (card*)malloc(sizeof(card));
   if (l != NULL) {
-    l->id = id;
-    l->priority = priority;
+    l->id = i;
+	if(pri > 10){ return NULL;}
+	else if(pri < 1){ return NULL;}
+    else{l->priority = pri;}
+    l->dateCreated = time;
+    l->dateDue = 0;
+    l->dateConclusion = 0;
     l->description = desc;
-    l->person = NULL; // not assigned yet
+    l->person = NULL;
   }
   return l;
 }
 
-void cardAssign(card *c, char *p) {
+void cardAssign(card* c, char* p) {
   c->person = p;
 }
 
-void cardPrint(card *c) {
-  puts(c->person);
-  puts(c->description);
+char* cardGetAssign(card* c){
+	return c -> person;
+}
+
+void cardSetDateCreated(card* c, long d){
+	c->dateCreated = d;
+}
+
+void cardSetDateDue(card* c, long d){
+	c->dateDue=d;
+}
+
+void cardSetDateConcluded(card* c, long d){
+	c->dateConclusion=d;
 }
